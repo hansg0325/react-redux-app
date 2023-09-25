2afe1ee0761acfddd546eb704409e3cf2f0e5383
hansg0325 <hansg0325@gmail.com>
2023-09-25 18:46:19 +0900

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux";
+import counter from "./reducers";
+import { subscribe } from "diagnostics_channel";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store);
+  console.log("next", action);
+  next(action);
+};
+
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+const store = createStore(rootReducer, middleware);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "USE_REDUX",
+});
+console.log(store.getState());
+
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
+    </React.StrictMode>
+  );
+
+render();
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
